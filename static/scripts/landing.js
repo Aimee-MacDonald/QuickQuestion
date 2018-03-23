f80b712568cf48b57ebb740f6cc111f18d4c7851
Aimée <admin@aimeemacdonald.com>
2018-03-23 12:43:06 +0000

    Next Poll

--- static/scripts/landing.js
+++ static/scripts/landing.js
@@ -1,25 +1,29 @@
 let landingPoll = document.getElementById("landing-poll").innerText;
+nextPoll();
 
-if(landingPoll){
-  updatePoll(JSON.parse(landingPoll));
-} else {
-  let request = new XMLHttpRequest();
-  request.onerror = () => {console.log("Error Response")};
-  request.open("get", "/api/getRandom", true);
-  request.withCredentials = true;
-  request.setRequestHeader('CSRF-Token', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
-  request.setRequestHeader('Content-Type', "Application/json");
-  request.onload = () => {
-    if(request.readyState === 4 && request.status === 200){
-      updatePoll(JSON.parse(request.responseText));
+function nextPoll(){
+  if(landingPoll){
+    updatePoll(JSON.parse(landingPoll));
+  } else {
+    let request = new XMLHttpRequest();
+    request.onerror = () => {console.log("Error Response")};
+    request.open("get", "/api/getRandom", true);
+    request.withCredentials = true;
+    request.setRequestHeader('CSRF-Token', document.querySelector('meta[name="csrf-token"]').getAttribute('content'));
+    request.setRequestHeader('Content-Type', "Application/json");
+    request.onload = () => {
+      if(request.readyState === 4 && request.status === 200){
+        updatePoll(JSON.parse(request.responseText));
+      };
     };
-  };
-  request.send();
+    request.send();
+  }
 }
 
 function updatePoll(polldata){
   document.getElementById("question").innerText = polldata.question;
   document.getElementById("pollid").innerText = polldata.pollid;
+  document.getElementById("answers").innerText = "";
 
   polldata.answers.forEach(i => {
     let newAnswer = document.createElement("li");
